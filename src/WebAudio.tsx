06d2d2c7fb7e2038409ceaf07b49da969bdf9202
robinbood <mercedesfarts@gmail.com>
2025-07-09 13:19:51 +0500

    initial commit

---
+++ src/WebAudio.tsx
@@ -0,0 +1,8 @@
+const WebAudio = () => {
+  // WOscillator on WebAudio  "this will go brrr in sine"
+  const oscillator = new AudioContext();
+
+  /* do you really neeed it? no  that's why i'm gonna complete it when i feel the need for it */
+};
+
+export default WebAudio;
